e46171da8a3f41efc899d0fb6a2df7f42a6ba2d2
Tut-k0 <Tut_k0@protonmail.com>
2022-04-23 11:35:36 -0500

    Project start commit.

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts."}
