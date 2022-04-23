19e585396c5a4e36c76dd46066f453d87d5fbb1c
Tut-k0 <Tut_k0@protonmail.com>
2022-04-23 14:23:21 -0500

    Add basic CRUD operations.
    * Added basic CRUD operations with data type validation via pydantic class.
    
    * Using hard coded data for tests/data currently, DB to be added soon.

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-from fastapi import FastAPI
+from random import randrange
+from typing import Optional
+
+from fastapi import FastAPI, status, HTTPException, Response
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [
+    {"title": "title of post 1", "content": "content of post 1", "id": 1},
+    {"title": "favourite foods", "content": "I like pizza.", "id": 2}]
+
+
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+
+def find_index_of_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
 
 
 @app.get("/")
@@ -10,4 +38,44 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts."}
+    return {"data": my_posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found.")
+    return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_of_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found.")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_of_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found.")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
