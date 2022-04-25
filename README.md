9671f4312f325071dd5ea115d32185076de9a5ff
Tut-k0 <Tut_k0@protonmail.com>
2022-04-24 22:22:16 -0500

    Refactor code/project structure.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WIP
+
+## Install/Run Instructions
+```bash
+pip install requirements.txt
+python -m uvicorn app.main:app
+```
+Go to http://127.0.0.1:8000/docs once running for documentation.
--- app/main.py
+++ app/main.py
@@ -58,16 +58,6 @@ def get_post(id: int):
     return {"post_detail": post}
 
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    index = find_index_of_post(id)
-    if index is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id {id} was not found.")
-    my_posts.pop(index)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
     index = find_index_of_post(id)
@@ -79,3 +69,13 @@ def update_post(id: int, post: Post):
     post_dict['id'] = id
     my_posts[index] = post_dict
     return {"data": post_dict}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_of_post(id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found.")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
