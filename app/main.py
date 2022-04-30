fe79d9d7bb9dcc2dc88efaf03d76732fcba06947
Tut-k0 <Tut_k0@protonmail.com>
2022-04-30 09:52:14 -0500

    Remove code comments from raw SQL.

--- app/main.py
+++ app/main.py
@@ -32,8 +32,6 @@ def root():
 @app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    # cursor.execute("""SELECT * FROM posts ORDER BY id;""")
-    # posts = cursor.fetchall()
     return posts
 
 
@@ -44,20 +42,12 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    # cursor.execute("""INSERT INTO posts (title, content, published)
-    #                VALUES (%s, %s, %s)
-    #                RETURNING *;""",
-    #                (post.title, post.content, post.published,))
-    # new_post = cursor.fetchone()
-    # conn.commit()
     return new_post
 
 
 @app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
-    # cursor.execute("""SELECT * FROM posts WHERE id = %s;""", (str(id),))
-    # post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id {id} was not found.")
@@ -67,14 +57,6 @@ def get_post(id: int, db: Session = Depends(get_db)):
 @app.put("/posts/{id}", response_model=schemas.Post)
 def update_post(id: int, post: schemas.PostCreate,
                 db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET title = %s,
-    #                                    content = %s,
-    #                                    published = %s
-    #                   WHERE id = %s
-    #                   RETURNING *;""",
-    #                (post.title, post.content, post.published, str(id),))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
     post_query = db.query(models.Post).filter(models.Post.id == id)
     update_post = post_query.first()
     if update_post is None:
@@ -88,10 +70,6 @@ def update_post(id: int, post: schemas.PostCreate,
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id)
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *;""",
-    #                (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
     if post.first() is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id {id} was not found.")
